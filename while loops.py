a3ec9d38d93d42fc689359a436944ff7befaa021
MesbahUddinMohd <111533033+MesbahUddinMohd@users.noreply.github.com>
2022-08-21 13:15:16 +0600

    Add files via upload
     while and do while loops.
    Using Second code  while and do while loops.

---
+++ while loops.py
@@ -0,0 +1,5 @@
+number = 10
+while number < 99:
+    print(f"Number is {number}!")
+    number = number + 10
+## Python does not have built-in functionality to explicitly create a do while loop like other languages.
